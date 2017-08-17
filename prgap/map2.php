bd24a45469009e4d695560516a2880254e378ef5
Jim White <jbwhite2@ncsu.edu>
2017-08-17 14:44:56 -0400

    debug

--- prgap/map2.php
+++ prgap/map2.php
@@ -140,7 +140,7 @@ function send_ajax(){
 	var richness_species = $('#richness_species_ajax').val();
 	//id number for job to check if create map object fails
 	//var job_id = Math.floor(Math.random() * 1000000000);
-
+console.log("testing");
 	$('.mapimage').hide();
 	$('#loader_gif').show();
 	$.ajax({
